ad6cc6c46a4ea403dc08f7fd19f739f99ccc1491
Anirruddha Banerjee <i.m.anirruddha.banerjee@gmail.com>
2026-08-31 23:42:48 +0530

    Array: add explanatory comments to maximum circular sum subarray

--- gfg/list/advanced/7. maximum circular sum subarray.py
+++ gfg/list/advanced/7. maximum circular sum subarray.py
@@ -1,6 +1,6 @@
 # given a circular array of size n, find the maximum subarray sum of the non-empty subarray
 
-# MaxSub() is kaden's algorithm
+# MaxSub() is kaden's algorithm to find max subarray sum
 def MaxSub(arr):
     n = len(arr)
     res = arr[0]
@@ -12,14 +12,18 @@ def MaxSub(arr):
 
 def CMaxSub(arr):
     n = len(arr)
+    # normal max subarray sum
     max_normal = MaxSub(arr)
     if max_normal<0:
         return max_normal
 
+    # circular max subarray sum
+    # compute the sum of array
     sum = 0
     for i in range(0, n):
         sum = sum + arr[i]
+        # negating the array, to compute minimum subarray sum
         arr[i] = -arr[i]
-
+    # here MaxSub(negated_array) is computing the minimum subarray sum
     max_circular = sum + MaxSub(arr)
     return max(max_circular, max_normal)
